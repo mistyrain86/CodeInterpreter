ae0f6b2764fb4df1e24da8c21921418487ba49ae
Choi <mistyrain86@gmail.com>
2026-06-04 10:26:04 +0900

    [feature] 프로젝트 파일 구성 및 진입점 추가
    - vcxproj에 헤더 파일 5개(Value.h, ILexer/IParser/IChecker/IInterpreter.h) 등록
    - main.cpp 진입점 추가 (Phase 0 빌드 확인용)
    - Debug x64 빌드 정상 확인

---
+++ CodeInterpreter/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+int main() {
+#ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+#endif
+    std::cout << "CodeFab - Phase 0 빌드 확인용\n";
+    return 0;
+}
