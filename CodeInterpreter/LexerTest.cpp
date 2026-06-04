ebab1d61b81e2245e2a367f0200fe46f50a8479f
SsemiW <asdfhck@naver.com>
2026-06-04 15:40:08 +0900

    [feature] 전체 단일 문자 토큰 인식 기능 및 공백 처리 추가   - Lexer.cpp: switch문 기반 루프로 ), {, }, ; 토큰 인식 추가 & 공백·탭·캐리지리턴 스킵 & 개행 시 m_line 증가 처리   - LexerTest.cpp: AllSingleCharTokens 테스트 케이스 추가

--- CodeInterpreter/LexerTest.cpp
+++ CodeInterpreter/LexerTest.cpp
@@ -16,3 +16,15 @@ TEST(LexerTest, LeftParenToken) {
     EXPECT_EQ(token_array[0].line, 1);
     EXPECT_EQ((int)token_array.size(), 2);
 }
+
+TEST(LexerTest, AllSingleCharTokens) {
+    Lexer lexer;
+    auto token_array = lexer.tokenize("( ) { } ;");
+    EXPECT_EQ(token_array[0].type, TokenType::LEFT_PAREN);
+    EXPECT_EQ(token_array[1].type, TokenType::RIGHT_PAREN);
+    EXPECT_EQ(token_array[2].type, TokenType::LEFT_BRACE);
+    EXPECT_EQ(token_array[3].type, TokenType::RIGHT_BRACE);
+    EXPECT_EQ(token_array[4].type, TokenType::SEMICOLON);
+    EXPECT_EQ(token_array[5].type, TokenType::END_OF_FILE);
+    EXPECT_EQ((int)token_array.size(), 6);
+}
