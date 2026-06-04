0e5193d5fdd8fea32ff621059a542e2292b46022
Choi <mistyrain86@gmail.com>
2026-06-04 13:02:53 +0900

    [refactoring] EnvironmentTest에 Fixture 적용
    각 테스트마다 반복되던 Environment 생성을 EnvironmentFixture로 추출

--- CodeInterpreter/EnvironmentTest.cpp
+++ CodeInterpreter/EnvironmentTest.cpp
@@ -1,27 +1,27 @@
 #include <gtest/gtest.h>
 #include "Environment.h"
-#include "TestUtils.h"
 
 static Token tok(std::string name, int line = 1) {
     return Token{TokenType::IDENTIFIER, std::move(name), std::monostate{}, line};
 }
 
-TEST(EnvironmentTest, DefineAndGet) {
+class EnvironmentFixture : public ::testing::Test {
+protected:
     Environment env;
+};
+
+TEST_F(EnvironmentFixture, DefineAndGet) {
     env.define("a", Value{10.0});
     EXPECT_DOUBLE_EQ(std::get<double>(env.get(tok("a"))), 10.0);
 }
-TEST(EnvironmentTest, DefineString) {
-    Environment env;
+TEST_F(EnvironmentFixture, DefineString) {
     env.define("s", Value{std::string("hi")});
     EXPECT_EQ(std::get<std::string>(env.get(tok("s"))), "hi");
 }
-TEST(EnvironmentTest, UndefinedVariable_Throws) {
-    Environment env;
+TEST_F(EnvironmentFixture, UndefinedVariable_Throws) {
     EXPECT_THROW(env.get(tok("x", 5)), std::runtime_error);
 }
-TEST(EnvironmentTest, ErrorMsg_HasLineAndName) {
-    Environment env;
+TEST_F(EnvironmentFixture, ErrorMsg_HasLineAndName) {
     try { env.get(tok("missing", 7)); FAIL(); }
     catch (const std::runtime_error& e) {
         std::string msg = e.what();
@@ -29,13 +29,11 @@ TEST(EnvironmentTest, ErrorMsg_HasLineAndName) {
         EXPECT_NE(msg.find("missing"), std::string::npos);
     }
 }
-TEST(EnvironmentTest, Assign_UpdatesValue) {
-    Environment env;
+TEST_F(EnvironmentFixture, Assign_UpdatesValue) {
     env.define("a", Value{1.0});
     env.assign(tok("a"), Value{99.0});
     EXPECT_DOUBLE_EQ(std::get<double>(env.get(tok("a"))), 99.0);
 }
-TEST(EnvironmentTest, Assign_Undefined_Throws) {
-    Environment env;
+TEST_F(EnvironmentFixture, Assign_Undefined_Throws) {
     EXPECT_THROW(env.assign(tok("x"), Value{1.0}), std::runtime_error);
 }
