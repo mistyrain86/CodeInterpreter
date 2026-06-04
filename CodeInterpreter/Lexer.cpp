4750433aeb5c608cf758c33c431dc76ee885c4fb
SsemiW <asdfhck@naver.com>
2026-06-04 14:26:52 +0900

    [feature] Lexer 구현체 및 EOF 검증 Unit Test 추가   - Lexer.h / Lexer.cpp: ILexer 인터페이스를 구현하는 Lexer 클래스 작성 & END_OF_FILE   토큰을 반환하는 tokenize 함수 구현   - LexerTest.cpp: EOFAlwaysAppended 테스트 케이스 추가

---
+++ CodeInterpreter/Lexer.cpp
@@ -0,0 +1,6 @@
+﻿#include "Lexer.h"
+
+std::vector<Token> Lexer::tokenize(const std::string& source) {
+    m_tokens.emplace_back(TokenType::END_OF_FILE, "", std::monostate{}, 1);
+    return m_tokens;
+}
---
+++ CodeInterpreter/LexerTest.cpp
@@ -0,0 +1,9 @@
+﻿#include <gtest/gtest.h>
+#include "Lexer.h"
+
+TEST(LexerTest, EOFAlwaysAppended) {
+    Lexer lexer;
+    auto token_array = lexer.tokenize("");
+    ASSERT_EQ((int)token_array.size(), 1);
+    EXPECT_EQ(token_array[0].type, TokenType::END_OF_FILE);
+}
