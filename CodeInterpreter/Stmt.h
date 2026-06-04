c8bcd553128d6681c6a4b71a8591f3e1dae4cbb2
Choi <mistyrain86@gmail.com>
2026-06-05 08:30:05 +0900

    [refactoring] Expr/Stmt에 accept() 추가 — Visitor 패턴 AST 적용
    각 노드에 accept(ExprVisitor&) / accept(StmtVisitor&) 구현
    새 노드 추가 시 accept() 하나만 구현하면 자동으로 Visitor 지원

--- CodeInterpreter/Stmt.h
+++ CodeInterpreter/Stmt.h
@@ -3,18 +3,24 @@
 #include <vector>
 #include "Expr.h"
 #include "Token.h"
+#include "StmtVisitor.h"
 
-struct Stmt { virtual ~Stmt() = default; };
+struct Stmt {
+    virtual ~Stmt() = default;
+    virtual void accept(StmtVisitor& v) = 0;
+};
 using StmtPtr = std::unique_ptr<Stmt>;
 
 struct ExprStmt : Stmt {
     ExprPtr expression;
     explicit ExprStmt(ExprPtr e) : expression(std::move(e)) {}
+    void accept(StmtVisitor& v) override { v.visitExprStmt(*this); }
 };
 
 struct PrintStmt : Stmt {
     ExprPtr expression;
     explicit PrintStmt(ExprPtr e) : expression(std::move(e)) {}
+    void accept(StmtVisitor& v) override { v.visitPrintStmt(*this); }
 };
 
 struct VarStmt : Stmt {
@@ -22,11 +28,13 @@ struct VarStmt : Stmt {
     ExprPtr initializer;
     VarStmt(Token n, ExprPtr i)
         : name(std::move(n)), initializer(std::move(i)) {}
+    void accept(StmtVisitor& v) override { v.visitVarStmt(*this); }
 };
 
 struct BlockStmt : Stmt {
     std::vector<StmtPtr> statements;
     explicit BlockStmt(std::vector<StmtPtr> s) : statements(std::move(s)) {}
+    void accept(StmtVisitor& v) override { v.visitBlockStmt(*this); }
 };
 
 struct IfStmt : Stmt {
@@ -37,6 +45,7 @@ struct IfStmt : Stmt {
         : condition(std::move(c))
         , thenBranch(std::move(t))
         , elseBranch(std::move(e)) {}
+    void accept(StmtVisitor& v) override { v.visitIfStmt(*this); }
 };
 
 struct ForStmt : Stmt {
@@ -49,4 +58,5 @@ struct ForStmt : Stmt {
         , condition(std::move(c))
         , increment(std::move(inc))
         , body(std::move(b)) {}
+    void accept(StmtVisitor& v) override { v.visitForStmt(*this); }
 };
