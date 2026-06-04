a53976dac360e194be0ad685f0bc6fd1134b44a2
donghyun.yoon <ydh1305@naver.com>
2026-06-04 15:59:23 +0900

    [refactoring] Parser: 이항/단항 테스트 null 체크 패턴 일관성 적용
    BoolTrue/BoolFalse에 추가된 ASSERT_NE 패턴을
    LeftAssociativity/Comparison_Less/UnaryBang에도 통일 적용

--- CodeInterpreter/ParserTest.cpp
+++ CodeInterpreter/ParserTest.cpp
@@ -92,6 +92,7 @@ TEST(ParserUnit, LeftAssociativity) {
                         t(TokenType::NUMBER,"3",3.0),   semi(), eof()});
     auto* outer = dynamic_cast<BinaryExpr*>(
         dynamic_cast<ExprStmt*>(stmts[0].get())->expression.get());
+    ASSERT_NE(outer, nullptr);
     EXPECT_NE(dynamic_cast<BinaryExpr*>(outer->left.get()), nullptr);
 }
 TEST(ParserUnit, Comparison_Less) {
@@ -99,6 +100,7 @@ TEST(ParserUnit, Comparison_Less) {
                         t(TokenType::NUMBER,"2",2.0), semi(), eof()});
     auto* bin = dynamic_cast<BinaryExpr*>(
         dynamic_cast<ExprStmt*>(stmts[0].get())->expression.get());
+    ASSERT_NE(bin, nullptr);
     EXPECT_EQ(bin->op.type, TokenType::LESS);
 }
 
@@ -115,6 +117,7 @@ TEST(ParserUnit, UnaryBang) {
                         t(TokenType::KW_TRUE,"true"), semi(), eof()});
     auto* un = dynamic_cast<UnaryExpr*>(
         dynamic_cast<ExprStmt*>(stmts[0].get())->expression.get());
+    ASSERT_NE(un, nullptr);
     EXPECT_EQ(un->op.type, TokenType::BANG);
 }
 
