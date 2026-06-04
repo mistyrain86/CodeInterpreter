8e7db0726f6b0ff9f4e8bf74ccfbb6ea99c2fd81
Choi <mistyrain86@gmail.com>
2026-06-04 14:32:46 +0900

    [feature] Interpreter: 이항 연산(BinaryExpr) 구현 및 테스트 추가
    - 산술: +, -, *, / (0 나누기 RuntimeError)
    - 문자열 연결: + (두 string)
    - 비교: >, >=, <, <=, ==, !=
    - 타입 불일치 RuntimeError
    - 테스트 9개 추가, 전체 30개 통과

--- CodeInterpreter/InterpreterTest.cpp
+++ CodeInterpreter/InterpreterTest.cpp
@@ -5,6 +5,10 @@
 static Token opTok(TokenType t, std::string lex, int line = 1) {
     return Token{t, std::move(lex), std::monostate{}, line};
 }
+static ExprPtr bin(ExprPtr l, TokenType op, std::string lex, ExprPtr r) {
+    return std::make_unique<BinaryExpr>(
+        std::move(l), Token{op, std::move(lex), std::monostate{}, 1}, std::move(r));
+}
 
 class InterpreterFixture : public ::testing::Test {
 protected:
@@ -38,4 +42,31 @@ TEST_F(InterpreterFixture, UnaryMinus_OnString_Throws) {
         std::make_unique<UnaryExpr>(opTok(TokenType::MINUS, "-"), litStr("oops"))));
     Interpreter interp;
     EXPECT_THROW(interp.interpret(stmts), RuntimeError);
+}
+
+// 이항 연산
+TEST_F(InterpreterFixture, Add)   { EXPECT_EQ(run(printStmt(bin(litNum(3),  TokenType::PLUS,  "+", litNum(4)))),  "7\n");  }
+TEST_F(InterpreterFixture, Sub)   { EXPECT_EQ(run(printStmt(bin(litNum(10), TokenType::MINUS, "-", litNum(3)))),  "7\n");  }
+TEST_F(InterpreterFixture, Mul)   { EXPECT_EQ(run(printStmt(bin(litNum(3),  TokenType::STAR,  "*", litNum(4)))),  "12\n"); }
+TEST_F(InterpreterFixture, Div)   { EXPECT_EQ(run(printStmt(bin(litNum(8),  TokenType::SLASH, "/", litNum(2)))),  "4\n");  }
+TEST_F(InterpreterFixture, StrConcat) {
+    EXPECT_EQ(run(printStmt(bin(litStr("Hi"), TokenType::PLUS, "+", litStr("!")))), "Hi!\n");
+}
+TEST_F(InterpreterFixture, CmpLess_True) {
+    EXPECT_EQ(run(printStmt(bin(litNum(1), TokenType::LESS,    "<", litNum(2)))), "true\n");
+}
+TEST_F(InterpreterFixture, CmpGreater_False) {
+    EXPECT_EQ(run(printStmt(bin(litNum(3), TokenType::GREATER, ">", litNum(5)))), "false\n");
+}
+TEST_F(InterpreterFixture, TypeMismatch_Throws) {
+    std::vector<StmtPtr> s;
+    s.push_back(std::make_unique<ExprStmt>(bin(litNum(1), TokenType::PLUS, "+", litStr("HI"))));
+    Interpreter i;
+    EXPECT_THROW(i.interpret(s), RuntimeError);
+}
+TEST_F(InterpreterFixture, DivByZero_Throws) {
+    std::vector<StmtPtr> s;
+    s.push_back(std::make_unique<ExprStmt>(bin(litNum(1), TokenType::SLASH, "/", litNum(0))));
+    Interpreter i;
+    EXPECT_THROW(i.interpret(s), RuntimeError);
 }
