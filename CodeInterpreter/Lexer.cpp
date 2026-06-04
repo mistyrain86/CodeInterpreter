72f8ec3205ae5f7a6f92456eebe35d3ca16bcbcc
SsemiW <asdfhck@naver.com>
2026-06-04 14:48:29 +0900

    [feature] LEFT_PAREN 단일 문자 토큰 인식   기능 추가   - Lexer.cpp: '(' 문자를 LEFT_PAREN 토큰으로   변환하는 로직 추가   - LexerTest.cpp: LeftParenToken 테스트   케이스 추가

--- CodeInterpreter/Lexer.cpp
+++ CodeInterpreter/Lexer.cpp
@@ -1,6 +1,10 @@
 ﻿#include "Lexer.h"
 
 std::vector<Token> Lexer::tokenize(const std::string& source) {
+    if (source[0] == '(') {
+        m_tokens.emplace_back(TokenType::LEFT_PAREN, "(", std::monostate{}, m_line);
+    }
+
     m_tokens.emplace_back(TokenType::END_OF_FILE, "", std::monostate{}, m_line);
     return m_tokens;
 }
--- CodeInterpreter/LexerTest.cpp
+++ CodeInterpreter/LexerTest.cpp
@@ -7,3 +7,12 @@ TEST(LexerTest, EOFAlwaysAppended) {
     ASSERT_EQ((int)token_array.size(), 1);
     EXPECT_EQ(token_array[0].type, TokenType::END_OF_FILE);
 }
+
+TEST(LexerTest, LeftParenToken) {
+    Lexer lexer;
+    auto token_array = lexer.tokenize("(");
+    EXPECT_EQ(token_array[0].type, TokenType::LEFT_PAREN);
+    EXPECT_EQ(token_array[0].lexeme, "(");
+    EXPECT_EQ(token_array[0].line, 1);
+    EXPECT_EQ((int)token_array.size(), 2);
+}
