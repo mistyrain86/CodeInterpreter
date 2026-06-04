2eb6172807d0ce4352962d95a08cd3be53ee8ac8
SsemiW <asdfhck@naver.com>
2026-06-04 15:31:28 +0900

    [refactoring] 토큰 생성 로직을 addToken 메서드로 추출   - 토큰 추가 시 반복되는 emplace_back 호출을 addToken 헬퍼 메서드로 캡슐화하여 코드 중복 제거   - Lexer.h: addToken(TokenType, std::string) 메서드 선언 추가   - Lexer.cpp: addToken 구현 및 기존 emplace_back 호출 대체

--- CodeInterpreter/Lexer.cpp
+++ CodeInterpreter/Lexer.cpp
@@ -2,9 +2,13 @@
 
 std::vector<Token> Lexer::tokenize(const std::string& source) {
     if (source[0] == '(') {
-        m_tokens.emplace_back(TokenType::LEFT_PAREN, "(", std::monostate{}, m_line);
+        addToken(TokenType::LEFT_PAREN, "(");
     }
 
-    m_tokens.emplace_back(TokenType::END_OF_FILE, "", std::monostate{}, m_line);
+    addToken(TokenType::END_OF_FILE, "");
     return m_tokens;
+}
+
+void Lexer::addToken(TokenType type, std::string singleChar) {
+    m_tokens.emplace_back(type, std::move(singleChar), std::monostate{}, m_line);
 }
