eda5017077150bf7a9a30c1103e8dfdf024f1904
Choi <mistyrain86@gmail.com>
2026-06-05 08:32:23 +0900

    [refactoring] Checker → StmtVisitor 구현체로 전환
    checkStmt dynamic_cast 체인 제거, stmt.accept(*this) 방식으로 전환
    visitXxxStmt() 메서드로 각 문장 처리 분리
    checkExpr는 void 반환 특성상 dynamic_cast 유지

--- CodeInterpreter/Checker.h
+++ CodeInterpreter/Checker.h
@@ -6,18 +6,27 @@
 #include "IChecker.h"
 #include "Expr.h"
 #include "Stmt.h"
+#include "StmtVisitor.h"
 
-class Checker : public IChecker {
+class Checker : public IChecker
+              , public StmtVisitor {
 public:
     Checker() = default;
     void check(const std::vector<StmtPtr>& stmts) override;
+
+    // StmtVisitor
+    void visitExprStmt (ExprStmt&)  override;
+    void visitPrintStmt(PrintStmt&) override;
+    void visitVarStmt  (VarStmt&)   override;
+    void visitBlockStmt(BlockStmt&) override;
+    void visitIfStmt   (IfStmt&)    override;
+    void visitForStmt  (ForStmt&)   override;
 
 private:
     std::vector<std::map<std::string, bool>> m_scopes;
 
     void checkStmts(const std::vector<StmtPtr>& stmts);
-    void checkStmt(Stmt* stmt);
-    void checkExpr(Expr* expr);
+    void checkExpr(Expr* expr);   // 표현식은 dynamic_cast 유지 (void 반환)
     void beginScope();
     void endScope();
     void declare(const Token& name);
