985f344c03b27dc61326e5e20f48c356ff5c99cb
SsemiW <asdfhck@naver.com>
2026-06-04 16:14:23 +0900

    [refactoring] Lexer를 커서 기반 구조로 개선  -  range-for 방식을 while + 커서 방식으로 전환하여 향후 lookahead 확장이 용이한 구조로 개선.   - Lexer.h: m_source, m_startIdx, m_currentIdx 멤버 추가                reset / scanToken / addToken 메서드 재선언   - Lexer.cpp: reset으로 상태 초기화, scanToken으로 문자 소비,                   addToken을 substr 기반으로 변경

--- CodeInterpreter/Lexer.cpp
+++ CodeInterpreter/Lexer.cpp
@@ -1,26 +1,42 @@
 ﻿#include "Lexer.h"
 
 std::vector<Token> Lexer::tokenize(const std::string& source) {
-    for (char singleChar : source) {
-        switch (singleChar) {
-            case '(': addToken(TokenType::LEFT_PAREN, "(");  break;
-            case ')': addToken(TokenType::RIGHT_PAREN, ")"); break;
-            case '{': addToken(TokenType::LEFT_BRACE, "{");  break;
-            case '}': addToken(TokenType::RIGHT_BRACE, "}"); break;
-            case ';': addToken(TokenType::SEMICOLON, ";");   break;
-            case ' ':
-            case '\r':
-            case '\t':
-                break;
-            case '\n': m_line++; break;
-            default: break;
-        }
+    reset(source);
+
+    while (m_currentIdx < (int)m_source.size()) {
+        m_startIdx = m_currentIdx;
+        scanToken();
     }
 
-    addToken(TokenType::END_OF_FILE, "");
+    m_tokens.emplace_back(TokenType::END_OF_FILE, "", std::monostate{}, m_line);
     return m_tokens;
 }
 
-void Lexer::addToken(TokenType type, std::string singleChar) {
+void Lexer::reset(const std::string& source) {
+    m_source = source;
+    m_tokens.clear();
+    m_startIdx = m_currentIdx = 0;
+    m_line = 1;
+}
+
+void Lexer::scanToken() {
+    char singleChar = m_source[m_currentIdx++];
+    switch (singleChar) {
+        case '(': addToken(TokenType::LEFT_PAREN);  break;
+        case ')': addToken(TokenType::RIGHT_PAREN); break;
+        case '{': addToken(TokenType::LEFT_BRACE);  break;
+        case '}': addToken(TokenType::RIGHT_BRACE); break;
+        case ';': addToken(TokenType::SEMICOLON);   break;
+        case ' ':
+        case '\r':
+        case '\t':
+            break;
+        case '\n': m_line++; break;
+        default: break;
+    }
+}
+
+void Lexer::addToken(TokenType type) {
+    std::string singleChar = m_source.substr(m_startIdx, m_currentIdx - m_startIdx);
     m_tokens.emplace_back(type, std::move(singleChar), std::monostate{}, m_line);
 }
