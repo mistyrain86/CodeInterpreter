5877b811136d6a2007a87457fae103c3f2fc28c7
SsemiW <asdfhck@naver.com>
2026-06-08 17:39:43 +0900

    [refactoring] 에러 헬퍼 함수 추출 및 매직 넘버 상수화
      반복되는 에러 메시지 생성 로직을 헬퍼 함수로 통합하고
      배열 크기 상한값을 이름 있는 상수로 추출함
    
      - Interpreter.cpp: runtimeErr 헬퍼 추가 및 전체 적용,
                             isExactlyZero 헬퍼로 0.0 비교 의도 명확화
      - Checker.cpp: checkErr 헬퍼 추가 및 throw 교체
      - ArrayBuiltin.cpp: 매직 넘버 1000000을 MAX_ARRAY_SIZE 상수로 교체

--- CodeInterpreter/src/interpreter/ArrayBuiltin.cpp
+++ CodeInterpreter/src/interpreter/ArrayBuiltin.cpp
@@ -2,13 +2,15 @@
 #include "RuntimeError.h"
 #include "Value.h"
 
+static constexpr int MAX_ARRAY_SIZE = 1'000'000;
+
 Value ArrayBuiltin::call(Interpreter&, const std::vector<Value>& args) {
     if (!std::holds_alternative<double>(args[0]))
         throw RuntimeError("런타임 오류: 배열 크기는 숫자여야 합니다.");
     int n = static_cast<int>(std::get<double>(args[0]));
     if (n < 0)
         throw RuntimeError("런타임 오류: 배열 크기는 0 이상이어야 합니다.");
-    if (n > 1000000)
+    if (n > MAX_ARRAY_SIZE)
         throw RuntimeError("런타임 오류: 배열 크기가 너무 큽니다.");
     return Value{std::make_shared<std::vector<Value>>(
         n, Value{std::monostate{}})};
