a76e5a9af95efa3885c6a81a67f366bb5335bfdb
lisanghyun <lisanghyun@gmail.com>
2026-06-08 10:57:00 +0900

    [fix] execute() 깊이 카운터 누락 버그 수정
      ReturnSignal/RuntimeError 예외 발생 시 m_executeDepth--가 실행되지 않아 next 커맨드의 깊이 계산이 틀어지는 버그 수정.
    
      - RAII Guard 도입으로 예외 경로에서도 m_executeDepth-- 보장

--- CodeInterpreter/src/interpreter/Interpreter.cpp
+++ CodeInterpreter/src/interpreter/Interpreter.cpp
@@ -55,9 +55,9 @@ Value Interpreter::evaluate(Expr& expr) {
 
 void Interpreter::execute(Stmt& stmt) {
     m_executeDepth++;
+    struct Guard { int& d; ~Guard() { --d; } } guard{m_executeDepth};
     if (m_stmtHook) m_stmtHook(stmt);
     stmt.accept(*this);
-    m_executeDepth--;
 }
 
 void Interpreter::executeBlock(const std::vector<StmtPtr>& stmts,
