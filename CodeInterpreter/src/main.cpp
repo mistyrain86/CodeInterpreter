d9b640daf70f266248b0870df21ad98d4dc2993e
donghyun.yoon <ydh1305@naver.com>
2026-06-08 09:20:28 +0900

    [fix] main.cpp / TestMain.cpp: #if 가드 제거, ExcludedFromBuild에 일임
    Release 모드에서 메뉴 대신 다른 구문이 실행되던 문제 수정.
    
    기존에는 ExcludedFromBuild와 #if 가드가 중복으로 파일 포함 여부를 제어하여
    컴파일러 전처리 타이밍에 따라 의도치 않은 코드가 실행되는 문제 발생.
    
    - src/main.cpp: #if !_DEBUG 가드 제거, main() 항상 컴파일
    - test/TestMain.cpp: #if _DEBUG 가드 제거, gtest main 항상 컴파일
    - 구성별 포함/제외는 vcxproj ExcludedFromBuild 단독으로 제어
      · Debug: main.cpp 제외, TestMain.cpp 포함 → gtest 실행
      · Release: TestMain.cpp 제외, main.cpp 포함 → 메뉴 실행

--- CodeInterpreter/src/main.cpp
+++ CodeInterpreter/src/main.cpp
@@ -8,7 +8,6 @@
 #include <windows.h>
 #endif
 
-#if !_DEBUG
 int main() {
 #ifdef _WIN32
     SetConsoleOutputCP(CP_UTF8);
@@ -52,20 +51,8 @@ int main() {
             std::cout << "\n아직 미구현된 기능입니다.\n\n";
             break;
 
-            std::cout << "실행할 파일명을 입력하세요 (예: test.txt): ";
-            std::cin >> filename;
-            std::cout << filename << " 파일을 실행합니다...\n\n";
-            shell.runFile(filename);
-            break;
-
         case 3:
             std::cout << "\n아직 미구현된 기능입니다.\n\n";
-            break;
-
-            std::cout << "디버그할 파일명을 입력하세요 (예: test.txt): ";
-            std::cin >> filename;
-            std::cout << filename << " 파일의 디버깅을 시작합니다...\n\n";
-            shell.runDebug(filename);
             break;
 
         case 4:
@@ -80,4 +67,3 @@ int main() {
 
     return 0;
 }
-#endif
--- CodeInterpreter/test/TestMain.cpp
+++ CodeInterpreter/test/TestMain.cpp
@@ -1,9 +1,7 @@
 ﻿#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
-#if _DEBUG
 int main(int argc, char** argv) {
     ::testing::InitGoogleMock(&argc, argv);
     return RUN_ALL_TESTS();
 }
-#endif
